566481efdadafd6590a69efad09aad0ed42a10b4
awoelf <98348782+awoelf@users.noreply.github.com>
2022-12-05 10:46:47 -0600

    completed contact component

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import './styles/App.css'
 // Component imports
 import AppHeader from './components/AppHeader';
 import AppFooter from './components/AppFooter';
-import Resume from './components/Resume';
+// import Resume from './components/Resume';
 // import Portfolio from './components/Portfolio';
-
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className='App h-100 d-flex flex-column'>
       <AppHeader />
-      <Resume />
+      <Contact />
       <AppFooter />
     </div>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,28 @@
-function Contact() {
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
+function Contact() {
+  return (
+    <div className='d-flex flex-column col-3 mx-auto'>
+      <h4 className="text-center">Contact</h4>
+      <div className='box d-flex flex-column align-items-center pt-3'>
+        <p className="text-center">
+            Email is the best way to get in touch with me. Check out my GitHub profile for
+            all of the projects I'm working on!
+            </p>
+            <Breadcrumb>
+            <Breadcrumb.Item href="https://github.com/awoelf?tab=repositories">
+                <i className="bi bi-github" /> Github
+            </Breadcrumb.Item>
+            <Breadcrumb.Item href="https://www.linkedin.com/in/alexis-w-dev/">
+                <i className="bi bi-linkedin" /> LinkedIn
+            </Breadcrumb.Item>
+            <Breadcrumb.Item href="mailto:awoelf@outlook.com">
+                <i className="bi bi-envelope" /> Email
+            </Breadcrumb.Item>
+            </Breadcrumb>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
